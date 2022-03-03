ddd66672ba3eb5b1f054ab234fd850a8317affac
mtttia <tia369852@gmail.com>
2022-03-03 21:40:57 +0100

    updating filemanager

--- lib/class/FileManager.dart
+++ lib/class/FileManager.dart
@@ -2,21 +2,24 @@ import 'dart:io';
 //import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 
-abstract class BaseFile{
+abstract class BaseFile {
   String path;
   String _name;
-  set name(String value){
-    if(value.contains('/') || value.contains('\\')){
+  FileManager? father;
+  set name(String value) {
+    if (value.contains('/') || value.contains('\\')) {
       throw Exception('file name not valid');
     }
     _name = value;
   }
-  String get name{
+
+  String get name {
     return _name;
   }
+
   FileSystemEntity? ref;
 
-  BaseFile(this.path, this._name, this.ref){
+  BaseFile(this.path, this._name, this.ref, this.father) {
     name = _name;
   }
 
@@ -24,39 +27,41 @@ abstract class BaseFile{
   Future<void> save();
 }
 
-class TextFile extends BaseFile{
+class TextFile extends BaseFile {
   String content;
-  TextFile(String path, String name, FileSystemEntity? ref, this.content) : super(path, name, ref);
-  TextFile.create(String currentPath, String name) : content="", super('$currentPath/$name', name, null);
-  static fromFile(String currentPath, String url) async{
+  TextFile(String path, String name, FileSystemEntity? ref, FileManager? father,
+      this.content)
+      : super(path, name, ref, father);
+  TextFile.create(String currentPath, String name, FileManager? father)
+      : content = "",
+        super('$currentPath/$name', name, null, father);
+  static fromFile(String currentPath, String url, FileManager? father) async {
     try {
       File file = File(url);
       bool exist = await file.exists();
       if (!exist) {
         throw Exception("File not exist");
       }
-      
+
       String name = basename(file.path);
       // String name = file.path;
       String content = await file.readAsString();
-      return TextFile('$currentPath/$name', name, file, content);
+      return TextFile('$currentPath/$name', name, file, father, content);
     } catch (ex) {
       rethrow;
     }
   }
 
-
   @override
-  String toString(){
+  String toString() {
     return name;
   }
 
   @override
-  Future<void> export(String url) async{
-    if(ref != null){
+  Future<void> export(String url) async {
+    if (ref != null) {
       save();
-    }
-    else{
+    } else {
       File file = File('$url/$name');
       await file.create();
       ref = file;
@@ -66,24 +71,31 @@ class TextFile extends BaseFile{
 
   @override
   Future<void> save() async {
-    if(ref != null && ref is File && ref!.existsSync()){
+    if (ref != null && ref is File && ref!.existsSync()) {
       File cf = ref as File;
-      await cf.writeAsString(content);       
-    }
-    else{
+      await cf.writeAsString(content);
+    } else {
       throw Exception('cannot save the document');
     }
   }
 }
 
-class FileManager extends BaseFile{
+class FileManager extends BaseFile {
+  int id;
   List<TextFile> files;
   List<FileManager> folders;
-  FileManager(String path, String name, FileSystemEntity? ref, this.files, this.folders) : super(path, name, ref);
-  FileManager.create(String path, String name, FileSystemEntity? ref) : files=[], folders=[], super(path, name, ref);
+  FileManager(String path, String name, FileSystemEntity? ref,
+      FileManager father, this.files, this.folders)
+      : id=DateTime.now().millisecondsSinceEpoch,
+      super(path, name, ref, father);
+  FileManager.create(String path, String name, FileManager father)
+      : files = [],
+        folders = [],
+        id=DateTime.now().millisecondsSinceEpoch,
+        super(path, name, null, father);
 
-
-  static Future<FileManager> fromFolder(String currentPath, String url) async {
+  static Future<FileManager> fromFolder(
+      String currentPath, String url, FileManager father) async {
     try {
       Directory dir = Directory(url);
       bool exist = await dir.exists();
@@ -93,31 +105,74 @@ class FileManager extends BaseFile{
       String name = basename(dir.path);
       String path = '$currentPath/$name';
       var file = await dir.list().toList();
-      List<TextFile> files = [];
-      List<FileManager> folders = [];
+      FileManager temp = FileManager.create(path, name, father);
       for (var f in file) {
-        // print('${f.path} is ${f is File ? 'file' : f is Directory ? 'directory' : 'unknown'}');
         if (f is File) {
-          files.add(await TextFile.fromFile(path, f.path));
+          temp.importFile(f.path);
         }
         if (f is Directory) {
-          FileManager temp = await FileManager.fromFolder(path, f.path);
-          folders.add(temp);
+          temp.importFolder(f.path);
         }
       }
-      return FileManager(path, name, dir, files, folders);
-      
+      return temp;
     } catch (ex) {
       rethrow;
     }
   }
 
-  @override
-  Future<void> export(String url) async{
-    if(ref != null){
-      save();
+  bool nameOk(String name) {
+    for (var f in files) {
+      if (f.name == name) return false;
     }
-    else{
+    for (var f in folders) {
+      if (f.name == name) return false;
+    }
+    return true;
+  }
+
+  void createFile(String fileName) {
+    if (nameOk(fileName)) {
+      files.add(TextFile.create(path, fileName, this));
+    } else {
+      throw Exception('name already used');
+    }
+  }
+
+  void importFile(String url) async {
+    TextFile ts = await TextFile.fromFile(path, url, this);
+    if (nameOk(ts.name)) {
+      files.add(ts);
+    } else {
+      throw Exception('name already used');
+    }
+  }
+
+  void createFolder(String folderName) {
+    if (nameOk(folderName)) {
+      folders.add(FileManager.create(path, folderName, this));
+    } else {
+      throw Exception('name already used');
+    }
+  }
+
+  void importFolder(String url) async {
+    FileManager fm = await FileManager.fromFolder(path, url, this);
+    if (nameOk(fm.name)) {
+      folders.add(fm);
+    } else {
+      throw Exception('name already used');
+    }
+  }
+
+  void generateTree(){
+    
+  }
+
+  @override
+  Future<void> export(String url) async {
+    if (ref != null) {
+      save();
+    } else {
       Directory d = Directory('$url/$name');
       await d.create();
       ref = d;
@@ -128,11 +183,11 @@ class FileManager extends BaseFile{
   @override
   Future<void> save() async {
     try {
-      if(ref != null && ref is Directory && ref!.existsSync()){
-        for(TextFile f in files){
+      if (ref != null && ref is Directory && ref!.existsSync()) {
+        for (TextFile f in files) {
           await f.export(ref!.path);
         }
-        for(FileManager f in folders){
+        for (FileManager f in folders) {
           await f.export(ref!.path);
         }
       }
@@ -140,5 +195,4 @@ class FileManager extends BaseFile{
       rethrow;
     }
   }
-
 }
